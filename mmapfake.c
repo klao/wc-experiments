44f8416d1f58a42943f1efe30facac3fe8350109
Mihaly Barasz <klao@nilcons.com>
2020-11-24 23:59:22 +0100

    Fix mmapfake logic

--- mmapfake.c
+++ mmapfake.c
@@ -10,18 +10,16 @@ int main() {
   if (fstat(0, &st) < 0) {
     err(1, NULL);
   }
-  // printf("file size: %ld\n", st.st_size);
 
-  void *f = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, 0, 0);
+  char *f = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, 0, 0);
   if (f == MAP_FAILED) {
     err(1, NULL);
   }
 
-  // Do something with it.
+  // Do something with it, say sample every cache line:
   long long int checksum = 0;
-  for (int i = 0; i < st.st_size / 32; ++i) {
-    // sample every cache line:
-    checksum += ((long long int *)f)[i];
+  for (int i = 0; i < st.st_size; i += 64) {
+    checksum += *((long long int *)(f + i));
   }
   printf("%lld\n", checksum);
 }
